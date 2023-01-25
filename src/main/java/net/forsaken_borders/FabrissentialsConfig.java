1f400d01c7d812734fee2b31157434d24838017d
AaronVB <24834067+Aaron2550@users.noreply.github.com>
2023-01-25 17:44:32 +0100

    Remove MedMenu

--- src/main/java/net/forsaken_borders/FabrissentialsConfig.java
+++ src/main/java/net/forsaken_borders/FabrissentialsConfig.java
@@ -5,11 +5,5 @@ import eu.midnightdust.lib.config.MidnightConfig;
 public class FabrissentialsConfig extends MidnightConfig {
 
 	// Check out https://github.com/TeamMidnightDust/MidnightLib/wiki/Using-MidnightConfig for Info on how to use this Class
-	// We need to wait for our translation branch to be done, unless we wanna push English-Only until then
-	// FIXME: Wait for translation Branch
-
-	@Comment(centered = true) public static Comment enableAliasComment;
 	@Entry public static boolean enableUnbanAlias = true;
-
-
 }
